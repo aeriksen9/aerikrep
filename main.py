1b0c25b7045b81ac1fbffdc2d835fc358ceb8cf4
begreatkell <145169515+begreatkell@users.noreply.github.com>
2023-10-30 10:58:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,15 @@ def encode(password):
             encoded_password += char
     return encoded_password
 
+def decode(password):
+    decoded_password = ''
+    for char in password:
+        if char.isdigit():
+            new_digit = str(int(char) - 3)
+            decoded_password += new_digit
+        else:
+            decoded_password += char
+    return decoded_password
 menu()
 
 while True:
@@ -25,7 +34,8 @@ while True:
         print("Your password has been encoded and stored!")
     elif option == '2':
         if encoded_pass:
-            print(f'The encoded password is {encoded_pass} and the original password is {password}.')
+            decoded_password = decode(encoded_pass)
+            print(f'The encoded password is {encoded_pass} and the original password is {decoded_password}.')
         else:
             print("No password has been encoded yet!")
     elif option == '3':
